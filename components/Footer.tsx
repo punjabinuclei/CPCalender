1ebb0e7a146cc46981b1c6ef5fb76ec5d9cea8c7
punjabinuclei <guppi0204@gmail.com>
2023-12-25 20:49:18 +0530

    Add new custom fonts and dependencies

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import tw from 'twrnc';
+import { clsx } from 'clsx';
 
 
 const Footer: React.FC = () => {
     return (
         <View style={tw`bg-white border-2  w-full mx-[1rem] px-[1rem] py-[2rem]`}>
-            <Text style={tw`text-[4rem] text-center font-bold tracking-widest`}>FOOTER</Text>
+            <Text style={[tw`text-6xl text-center font-bold tracking-widest`, { fontFamily: 'Roboto-Black' }]}>FOOTER</Text>
         </View>
     );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import tw from 'twrnc';
 const Navbar: React.FC = () => {
     return (
         <View style={tw`bg-white border-2  w-full mx-[1rem] px-[1rem] py-[2rem]`}>
-            <Text style={tw`text-[4rem] text-center font-bold tracking-widest`}>HEADER</Text>
+            <Text style={[tw`text-[4rem] text-center  tracking-widest`, { fontFamily: 'Roboto-Black' }]}>HEADER</Text>
         </View>
     );
 };
